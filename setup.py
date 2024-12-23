59f7631ceb734911c8c898a474e8224dbc1421fa
Housam Kak <housam.kak20@gmail.com>
2024-12-23 16:49:04 +0400

    Update version in setup.py to 1.1.0 for map-content-tool

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="map-content-tool",
-    version="1.0.0",
+    version="1.1.0",
     py_modules=["mapContentTool"],
     install_requires=[], 
     entry_points={
